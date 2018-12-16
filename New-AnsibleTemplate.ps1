0a2cb8918934539b3423053de6b5cc65deee21ca
weiyentan <weiyen.tan@gmail.com>
2018-12-17 11:20:48 +1300

    add help in document

--- New-AnsibleTemplate.ps1
+++ New-AnsibleTemplate.ps1
@@ -1,16 +1,23 @@
 <#
 	.SYNOPSIS
-		A brief description of the New-AnsibleTemplate.ps1 file.
+		This script is used to create the scaffolding for Ansible templates for Ansible Roles and Ansible Modules.
 	
 	.DESCRIPTION
-		A description of the file.
+		This script uses Plaster to create the scaffolding for templates for Ansible. There are two  types of templating you can create. Roles or Modules
+		This script will produce the script into that destination path.
 	
-	.PARAMETER Destination
-		A description of the Destination parameter.
+	.PARAMETER destination
+		This is the destination path of where the template will be. 
 	
 	.PARAMETER type
-		A description of the type parameter.
+		This is the type of template that is to be created. It can accept two types of values: Roles or Modules. 
 	
+	.EXAMPLE
+		C:\PS> .\New-AnsibleTemplate.ps1 -Destination C:\Git\Ansible\Roles\configure-service -type Role
+		This will create an Ansible Role called configure-service in  C:\Git\Ansible\Roles\
+	.EXAMPLE
+		C:\PS> New-AnsibleTemplate -Destination C:\Git\Ansible\Roles\configure-service\library -type Module
+		This will create  the necessary templating for Ansible Modules and deploy them to  the C:\Git\Ansible\Roles\configure-service\library directory.
 	.NOTES
 		===========================================================================
 		Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.156
